e8300e8cb466e0c749d10e44638c29b682c5cb18
Fernando Sanchez <146203186+Fernandosanchez1609@users.noreply.github.com>
2025-04-20 08:05:43 +0200

    endpoint de prueba creado

---
+++ pj_backend/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pj_backend.Models.Database.Entities;
+using pj_backend.Services;
+using RegisterRequest = pj_backend.Models.Database.Dtos.RegisterRequest;
+
+namespace pj_backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+  private readonly UserService _userService;
+
+  public UserController(UserService userService)
+  {
+    _userService = userService;
+  }
+
+  [HttpPost("register")]
+  public async Task<ActionResult<User>> Register([FromBody] RegisterRequest request)
+  {
+    try
+    {
+      var newUser = await _userService.RegisterAsync(request);
+      return Ok(newUser); // Devuelve 200 OK con el usuario creado
+    }
+    catch (Exception ex)
+    {
+      return BadRequest(ex.Message);
+    }
+  }
+}
