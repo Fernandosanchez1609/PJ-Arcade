f0628fdff740d2f081df051557c23c7635c4a9f4
Fernando Sanchez <146203186+Fernandosanchez1609@users.noreply.github.com>
2025-04-24 23:37:08 +0200

    si

--- pj_front/src/components/WsConnector.js
+++ pj_front/src/components/WsConnector.js
@@ -1,4 +1,3 @@
-// src/components/WsConnector.js
 "use client";
 
 import { useEffect } from "react";
@@ -10,23 +9,16 @@ export default function WsConnector() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    // 1) Callback de mensajes
     const handleMessage = (msg) => {
       if (msg.Type === "onlineCount") {
         dispatch(setOnlineCount(msg.Data));
       }
     };
-
-    // 2) Conectar (solo crea 1 socket si no hay ninguno abierto o en proceso)
     connectWebSocket(handleMessage);
-
-    // 3) Asegurar cierre en recarga o cierre de pestaña
     const handleBeforeUnload = () => {
       disconnectWebSocket();
     };
     window.addEventListener("beforeunload", handleBeforeUnload);
-
-    // 4) Cleanup de React (al desmontar, e.g. cambio de ruta o Fast Refresh)
     return () => {
       disconnectWebSocket();
       window.removeEventListener("beforeunload", handleBeforeUnload);
