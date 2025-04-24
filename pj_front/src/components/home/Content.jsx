ab94cbde3aca4ad3e250b133b282c122f116470f
fernandosanchez1609 <fsanlag686@g.educaand.es>
2025-04-24 14:01:26 +0200

    cambio por un error de hidratation creo

--- pj_front/src/components/home/Content.jsx
+++ pj_front/src/components/home/Content.jsx
@@ -1,21 +1,28 @@
 "use client";
+
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import styles from "./Home.module.css";
 import { useAuth } from "@/hooks/useAuth";
+import styles from "./Home.module.css";
 
 export default function Content() {
     const { token } = useAuth();
     const onlineCount = useSelector((state) => state.online.count);
 
+
+    const [hasMounted, setHasMounted] = useState(false);
+    useEffect(() => {
+        setHasMounted(true);
+    }, []);
+
     return (
         <>
-            {token && (
+            {hasMounted && token && (
                 <div className={styles.titles}>
                     🟢 Usuarios en línea: {onlineCount}
                 </div>
             )}
             <div className={styles.content}>
-
                 <div>Aquí van los juegos</div>
                 <div>(Pablo trabaja)</div>
             </div>
