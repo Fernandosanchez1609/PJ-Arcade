c60b28541eb918923110c51fce92b895a236b217
pablorzdmngz <pablorzdmngz@gmail.com>
2025-04-24 19:29:29 +0200

    Creo GameRepository y el metodo GetAllGamesAsync

---
+++ pj_backend/Models/Database/Repositories/GameRepository.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using pj_backend.Models.Database.Entities;
+using pj_backend.Models.Database.Repositories;
+
+public class GameRepository : Repository<Game, int>
+{
+    private readonly AppDbContext _context;
+
+    public GameRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<ICollection<Game>> GetAllGamesAsync()
+    {
+        return await _context.Set<Game>().ToArrayAsync();
+    }
+
+}
